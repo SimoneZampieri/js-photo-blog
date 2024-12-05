d2c6480931b03df1f767adc4555712ef0bf0ecb0
SimoneZampieri <simonezampieri106@gmail.com>
2024-12-05 11:42:02 +0100

    [update] js file

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,27 @@
+//stabiliamo l'endpoint e il container in cui verranno inserite le card
 const endpoint = 'https://jsonplaceholder.typicode.com/photos?_limit=6'
+
+const container = document.getElementById('loaderCont')
+
+//ora dobbiamo creare una funzione che ci permetta di caricare delle card nell'html assieme alle rispettive foto al loro interno
+
+//per prima cosa stabiliamo la chiamata con axios
+
+axios.get(endpoint)
+  .then(response => {
+    container.innerHTML = ''
+    response.data.forEach(polaroid => printCard(polaroid))
+  })
+
+
+//denomino la funzione ''printCard'' e come parametro le passo ''card''
+function printCard(polaroid){
+
+    //dopo averla visionata in thunder client, la devo destrutturare
+    const {albumId, id, title, url, thumbnailUrl} = polaroid;
+    
+    //ora possiamo inserirle
+
+
+}
+
